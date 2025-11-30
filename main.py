eee4376d402f5773acc5bcac1674ac09cba5617f
Fabio Travaglia <fabio.travaglia@lanac.com.br>
2025-11-30 11:39:41 -0300

    debug falso

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from fakepinterest import app # __init__
 
 if __name__ == '__main__':
-    app.run(debug=True) # Toda alteração realizada no site sera atualziada no nosso site no ar
+    app.run(debug=False) # Toda alteração realizada no site sera atualziada no nosso site no ar
